0626fe16457292aecc2212f5dfda13bc4d1ec94e
Oliwia <64958530+oliwiazxc@users.noreply.github.com>
2020-06-01 18:44:18 +0200

    Update wybór kategorii i losowanie hasła.py

--- wybór kategorii i losowanie hasła.py
+++ wybór kategorii i losowanie hasła.py
@@ -99,12 +99,177 @@ def button_action():
     letter_x = 155
     letter_y = 550
     for letter in alfabet:
-        button = Radiobutton(main_window, text = letter, bg="yellow", fg="red", width=4, height=2,font=(None,15))
-        button.place(x=letter_x,y=letter_y)
+        button = Button(main_window, text = letter, bg="yellow", fg="red", width=4, height=2,font=(None,15))
+        button.place(x=letter_x, y=letter_y)
         if letter == 'M': # do momentu, w którym pojawi się na przyciku litera "M" przyciski wyświetlają się w miejscu x=155, y=620
             letter_x = 155
             letter_y = 620
         else: # przyciski z literami po literze "M" wyświetlają się w miejscu x=225(155+70), y=620
             letter_x += 70
+
+# no i teraz funkcje, które umożliwią przypisanie każdemu przyciskowi odpowiadającej mu litery
+# to lecim
+def letter_A():
+    global word
+    global guessed
+    if "A" in word and "A" not in guessed:
+        guessed.append("A") # jeżeli dana literka jest w wylosowanym słowie i nie ma jej na liście już zgadniętych liter to zostaje ona do niej dodana
+
+def letter_B():
+    global word
+    global guessed
+    if "B" in word and "B" not in guessed:
+        guessed.append("B")
+
+def letter_C():
+    global word
+    global guessed
+    if "C" in word and "C" not in guessed:
+        guessed.append("C")
+
+def letter_D():
+    global word
+    global guessed
+    if "D" in word and "D" not in guessed:
+        guessed.append("D")
+
+def letter_E():
+    global word
+    global guessed
+    if "E" in word and "E" not in guessed:
+        guessed.append("E")
+
+def letter_F():
+    global word
+    global guessed
+    if "F" in word and "F" not in guessed:
+        guessed.append("F")
+def letter_G():
+    global word
+    global guessed
+    if "G" in word and "G" not in guessed:
+        guessed.append("G")
+
+def letter_G():
+    global word
+    global guessed
+    if "G" in word and "G" not in guessed:
+        guessed.append("G")
+
+def letter_H():
+    global word
+    global guessed
+    if "H" in word and "H" not in guessed:
+        guessed.append("H")
+
+def letter_I():
+    global word
+    global guessed
+    if "I" in word and "I" not in guessed:
+        guessed.append("I")
+
+def letter_J():
+    global word
+    global guessed
+    if "J" in word and "J" not in guessed:
+        guessed.append("J")
+
+def letter_K():
+    global word
+    global guessed
+    if "K" in word and "K" not in guessed:
+        guessed.append("K")
+
+def letter_L():
+    global word
+    global guessed
+    if "L" in word and "L" not in guessed:
+        guessed.append("L")
+
+def letter_M():
+    global word
+    global guessed
+    if "M" in word and "M" not in guessed:
+        guessed.append("M")
+
+def letter_N():
+    global word
+    global guessed
+    if "N" in word and "N" not in guessed:
+        guessed.append("N")
+
+def letter_O():
+    global word
+    global guessed
+    if "O" in word and "O" not in guessed:
+        guessed.append("O")
+
+def letter_P():
+    global word
+    global guessed
+    if "P" in word and "P" not in guessed:
+        guessed.append("P")
+
+def letter_Q():
+    global word
+    global guessed
+    if "Q" in word and "Q" not in guessed:
+        guessed.append("Q")
+
+def letter_R():
+    global word
+    global guessed
+    if "R" in word and "R" not in guessed:
+        guessed.append("R")
+
+def letter_S():
+    global word
+    global guessed
+    if "S" in word and "S" not in guessed:
+        guessed.append("S")
+
+def letter_T():
+    global word
+    global guessed
+    if "T" in word and "T" not in guessed:
+        guessed.append("T")
+
+def letter_U():
+    global word
+    global guessed
+    if "U" in word and "U" not in guessed:
+        guessed.append("U")
+
+def letter_V():
+    global word
+    global guessed
+    if "V" in word and "V" not in guessed:
+        guessed.append("V")
+
+def letter_W():
+    global word
+    global guessed
+    if "W" in word and "W" not in guessed:
+        guessed.append("W")
+
+def letter_X():
+    global word
+    global guessed
+    if "X" in word and "X" not in guessed:
+        guessed.append("X")
+
+def letter_Y():
+    global word
+    global guessed
+    if "Y" in word and "Y" not in guessed:
+        guessed.append("Y")
+
+def letter_Z():
+    global word
+    global guessed
+    if "Z" in word and "Z" not in guessed:
+        guessed.append("Z")
+
+Letters = [letter_A(), letter_B(), letter_C(), letter_D(), letter_E(), letter_F(), letter_G(), letter_H(), letter_I(), letter_J(), letter_K(), letter_L(), letter_M(), letter_N(), letter_O(), letter_P(), letter_Q(), letter_R(), letter_S(), letter_T(), letter_U(), letter_V(), letter_W(), letter_X(), letter_Y(), letter_Z()]
 
 main_window.mainloop()
